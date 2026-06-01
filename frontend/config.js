cd2291e472200a1376ae0cb4a32f5d4723a2df5b
devaccessgit <managergithubaccess@outlook.com>
2026-06-01 16:37:05 -0300

    Configura API online do Render

--- frontend/config.js
+++ frontend/config.js
@@ -3,4 +3,4 @@
 // Produção: troque pela URL do backend publicado no Render, exemplo:
 // window.ALBUM_COPA_API_BASE = 'https://copa2026-api.onrender.com/api';
 
-window.ALBUM_COPA_API_BASE = 'http://localhost:5000/api';
+window.ALBUM_COPA_API_BASE = 'https://copa-do-mundo-2026.onrender.com/api';
